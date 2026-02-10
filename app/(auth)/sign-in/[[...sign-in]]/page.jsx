f4ff0984254fe5f271cbbd8f6179f9bf734a908a
Yasir941 <yasirazam941@gmail.come.com>
2026-02-10 23:12:46 +0400

    fix: remove stray text causing reference error

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,17 +1,9 @@
- import { SignIn } from '@clerk/nextjs'
-
-
+import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-
   return (
-
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
-
       <SignIn />
-
     </main>
-
   )
-
-} update
+}
